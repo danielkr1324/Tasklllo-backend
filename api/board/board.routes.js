efddf5a37c4f3f3df39a5c0cb24fcc4cec50de31
Daniel Krot <danielkr1324@gmail.com>
2023-09-26 17:12:31 +0300

    updated build files

--- api/board/board.routes.js
+++ api/board/board.routes.js
@@ -1,25 +1,25 @@
-const express = require('express')
+const express = require("express")
 const {
   requireAuth,
   requireAdmin,
-} = require('../../middlewares/requireAuth.middleware')
+} = require("../../middlewares/requireAuth.middleware")
 const {
   getBoards,
   getBoardById,
   addBoard,
   updateBoard,
   removeBoard,
-} = require('./board.controller')
+} = require("./board.controller")
 const router = express.Router()
 
 // middleware that is specific to this router
 // router.use(requireAuth)
 
-router.get('/', requireAuth, getBoards)
-router.get('/:id', getBoardById)
-router.post('/', addBoard)
-router.put('/:id', updateBoard)
-router.delete('/:id', removeBoard)
+router.get("/", requireAuth, getBoards)
+router.get("/:id", requireAuth, getBoardById)
+router.post("/", requireAuth, addBoard)
+router.put("/:id", updateBoard)
+router.delete("/:id", removeBoard)
 // router.delete('/:id', requireAuth, requireAdmin, removeBoard)
 
 module.exports = router
